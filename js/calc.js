7fef14147f40957536eb7676b1af255c3dc9de1c
Charlie de Freitas <ypph279@gmail.com>
2018-06-02 13:08:02 +0100

    moved into correct folders and finished js
    the javascript may still need some formating changes since whole numbers show as x.00

---
+++ js/calc.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $("#test").click(function () {
+        context();
+    })
+});
+
+function context() {
+    var gross = $("#defaultForm-gross");
+    var net = $("#defaultForm-net");
+    var rate = $("#vatCodeId");
+    var check = $("#is-gross");
+
+    if (check.is(":checked")) {
+        if (!net.val()) {
+            alert("you haven't set net");
+        } else {
+            setGross(rate.val(), net.val())
+        }
+    } else {
+        if (!gross.val()) {
+            alert("you haven't set gross");
+        } else {
+            setNet(rate.val(), gross.val());
+        }
+    }
+}
+
+function setGross(rate, net) {
+    var gross = $("#defaultForm-gross")
+    gross.val((parseFloat(net) + (rate * net)).toFixed(2));
+    // return net + (rate * net);
+}
+
+function setNet(rate, gross) {
+    var net = $("#defaultForm-net");
+    net.val((parseFloat(gross) / (1 + parseFloat(rate))).toFixed(2));
+}
+
+
